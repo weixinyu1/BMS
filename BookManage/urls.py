c57e856d07c84a28c542dd249d28c7968579acd0
haha <1368898903@qq.com>
2018-09-17 14:11:59 +0800

    新增个人展示功能

--- BookManage/urls.py
+++ BookManage/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from BookManage import views
 
-urlpatterns = {
+urlpatterns = [
     url(r'login/',views.login_view),
     url(r'register/',views.register_view),
     url(r'index/', views.index_view),
@@ -10,5 +10,4 @@ urlpatterns = {
     url(r'borrow/',views.borrow_view),
     url(r'allbook/',views.showall_view),
     url(r'contact/',views.contact_view),
-
-}
+]
--- BookManage/views.py
+++ BookManage/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect, response
 from django.shortcuts import render
 
 # Create your views here.
-from BookManage.models import User
+from BookManage.models import User, BookType,Book,admin,Userborrow
 
 
 def index_view(request):
@@ -13,7 +13,7 @@ def index_view(request):
         return HttpResponseRedirect('/login/')
     uname=request.POST.get('username','')
     upwd=request.POST.get('password','')
-    # print uname,upwd
+    print uname,upwd
     count = User.objects.filter(uname=uname, upwd=upwd).count()
     user=User.objects.get(uname=uname, upwd=upwd)
     if count == 1:
@@ -22,10 +22,15 @@ def index_view(request):
         return render(request,'index.html',{'user':user})
     else:
         return HttpResponse('登录失败！')
-
+#个人功能
 def person_view(request):
-    return render(request, 'person.html')
-
+    user = User.objects.all()
+    book = Book.objects.all()
+    ad = admin.objects.all()
+    userborrow = Userborrow.objects.all()
+    bookType = BookType.objects.all()
+    for u in user:
+            return render(request, 'person.html', {'u':u,'book':book,'ad':ad,'userborrow':userborrow,'bookType':bookType})
 
 def showall_view(request):
     return render(request, 'allbook.html')
@@ -52,13 +57,16 @@ def register_view(request):
         #接收数据
         uname = request.POST.get('username','')
         upwd=request.POST.get('password','')
-        # print uname,upwd
+        print uname,upwd
         insertUser(uname,upwd)
         return HttpResponseRedirect('/login/')
 
     return HttpResponse('注册失败')
+
+# 注册用户功能
 def insertUser(uname,upwd):
     try:
         user = User.objects.get(uname=uname, upwd=upwd)
     except User.DoesNotExist:
         user = User.objects.create(uname=uname,upwd=upwd)
+
